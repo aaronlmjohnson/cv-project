4b92993100b06575273fec0a73593f3d7ba9a667
Aaron <ajonaaronajon@yahoo.com>
2022-04-17 22:47:44 -0500

    able to store first name into state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import UserInfo from "./components/UserInfo";
+import uniqid from "uniqid";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user:{
+        generalInfo:{
+          firstName:'',
+          lastName:'',
+        },
+      },
+      generalInfo:{
+        firstName:'',
+        lastName:''
+      }   
+  };
+}
+
+  handleChange = (e) => {
+    this.setState({
+        generalInfo:{firstName:e.target.value}
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+        user:{
+          generalInfo: this.state.generalInfo
+        },
+        generalInfo: {firstName: ''}, 
+    });
+
+  };
+
+  render() {
+    const { firstName } = this.state.generalInfo;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="first-name">First Name</label>
+          <input
+            onChange={this.handleChange}
+            value={firstName}
+            type="text"
+            id="first-name"
+          />
+
+          <button type="submit">Add Task</button>
+        </form>
+        <UserInfo firstName={firstName} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/UserInfo.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+
+class UserInfo extends Component {
+  // constructor(props){
+  //   super(props);
+  // }
+
+  render(){
+    
+    const { firstName} = this.props;
+    
+    return(
+      <div>
+
+      </div>
+    )
+  }
+};
+
+export default UserInfo;
