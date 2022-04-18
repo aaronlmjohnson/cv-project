1da8863d14767157a9feeeb3bb4565d8850358f9
Aaron <ajonaaronajon@yahoo.com>
2022-04-18 14:04:46 -0500

    refactor GeneralForm input management

--- src/components/GeneralForm.js
+++ src/components/GeneralForm.js
@@ -2,43 +2,29 @@ import React, { Component} from "react";
 
 
 class GeneralForm extends Component{
+    generalInput(value, type, id, labelValue){
+        return(
+            <div>
+                <label htmlFor={id}>{labelValue}</label>
+                <input
+                    onChange={this.props.handleChange}
+                    value={value}
+                    type = {type}
+                    id= {id}
+                />
+            </div>
+        ) 
+    }
     render(){
         const { firstName, lastName, email, phone } = this.props;
 
         return (
             <div>
             <form onSubmit={this.props.onSubmitTask}>
-              <label htmlFor="first-name">First Name</label>
-              <input
-                onChange={this.props.handleChange}
-                value={firstName}
-                type="text"
-                id="first-name"
-              />
-    
-              <label htmlFor="last-name">Last Name</label>
-              <input
-                onChange={this.props.handleChange}
-                value={lastName}
-                type="text"
-                id="last-name"
-              />
-    
-              <label htmlFor="email">e-mail</label>
-              <input
-                onChange={this.props.handleChange}
-                value={email}
-                type="email"
-                id="email"
-              />
-    
-              <label htmlFor="phone">phone number</label>
-              <input
-                onChange={this.props.handleChange}
-                value={phone}
-                type="phone"
-                id="phone"
-              />
+                {this.generalInput(firstName, "text", "first-name", "First Name")} 
+                {this.generalInput(lastName, "text", "last-name", "Last Name")}
+                {this.generalInput(email, "email", "email", "E-mail")}
+                {this.generalInput(phone, "phone", "phone", "Phone Number")}
               <button type="submit">Submit</button>
             </form>
           </div>
