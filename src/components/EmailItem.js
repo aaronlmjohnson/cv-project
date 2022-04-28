3b143b3acad00a255c5ca518f5d344bef7273f4a
Aaron <ajonaaronajon@yahoo.com>
2022-04-28 03:20:32 -0500

    added EmailItem component

--- src/components/EmailItem.js
+++ src/components/EmailItem.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const TextItem = (props)=>{
+const EmailItem = (props)=>{
     return( 
         <li class="form-item">
             <label htmlFor={props.name}> {props.value} </label>
-            <input type="text" id ={props.id} name={props.name}/>
+            <input type="email" id ={props.id} name={props.name}/>
         </li>
     );
 }
 
-export default TextItem;
+export default EmailItem;
--- src/components/GeneralInfoForm.js
+++ src/components/GeneralInfoForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import EmailItem from "./EmailItem";
 import FormHeader from "./FormHeader";
 import TextItem from "./TextItem";
 
@@ -6,9 +7,11 @@ const GeneralInfoForm = ()=>{
     return(
         <div id="general-info-form-container">
             <FormHeader id="general-info-header" value="General Information"/>
-            <form id= "general-info-form">
+            <form id="general-info-form">
                 <ul id="general-info-form-items">
                     <TextItem id={"full-name"} name={"fullName"} value={"Full Name:"}/>
+                    <EmailItem id={"email"} name={"email"} value={"Email:"}/>
+                    
                 </ul>    
             </form>
         </div> 
